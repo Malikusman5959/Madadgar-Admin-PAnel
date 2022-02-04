e06f3a095d8c6bfa8b6cd6cc5507536c34439125
malikusman <malikusman5959@gmail.com>
2022-02-04 19:52:05 +0500

    first

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import {AdminHome} from "./components/AdminHome"
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <AdminHome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TopBar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+function TopBar({heading , searchKey , setsearchKey}) {
+
+
+    return (
+        <div style={{backgroundColor : '#ffffff' , width : '100%' , height : '100px' , display : 'flex' , flexDirection : 'row' , justifyContent : 'space-between' , alignItems : 'center'}}>
+           
+            <div style={{marginLeft : '42%' , display : 'flex' , flexDirection : 'row' , justifyContent : 'space-between' , alignItems : 'center'}}>
+            <img style={{height : '40px' , marginRight : '10px'}} src={heading.icon}/>
+            <p style={{color : '#2575c0' , fontSize : '30px' , fontWeight : 'bold' , letterSpacing : '3px'}}> {heading.name.toUpperCase()} </p>
+            </div>
+
+              {/* Selectors */}
+               { heading.name == 'Dashboard' ? null : <div style={{marginRight : '15px'}}>
+               <input style={{height : '28px',  width: '180px', marginRight: '5px', border: '2px solid #2575c0', borderRadius: '8px' }} type="text" value={searchKey} onChange={(e) => { setsearchKey(e.target.value) }} />
+                    <button style={{height : '32px',fontSize: '14px', fontFamily: 'Roboto, sans-serif', color: '#ffffff', width: '70px', backgroundColor: '#2575c0', border: '2px solid #2575c0', borderRadius: '8px' }} type="button" onClick={() => { console.log(true) }}> Search </button>
+               </div>   }  
+
+        </div>
+    )
+}
+
+export default TopBar
